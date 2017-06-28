ff72e759497d5013838646f99f3da3acfc79930c
66ccff <1154212237@qq.com>
2017-06-28 19:00:39 +0800

    test

---
+++ src/lib/dnssdint.h
@@ -0,0 +1,22 @@
+#ifndef DNSSDINT_H
+#define DNSSDINT_H
+
+#define RAOP_TXTVERS "1"
+#define RAOP_CH "2"             /* Audio channels: 2 */
+#define RAOP_CN "0,1,2,3"           /* Audio codec: PCM, ALAC */
+#define RAOP_DA "true"
+#define RAOP_ET "0,1"           /* Encryption type: none, RSA */
+#define RAOP_FT "0x5A7FFFF7,0x1E"
+#define RAOP_MD "0,1,2"
+#define RAOP_PW "false"
+#define RAOP_SV "false"
+#define RAOP_SR "44100"
+#define RAOP_SS "16"            /* Sample size: 16 */
+#define RAOP_TP "UDP"
+#define RAOP_VS "220.68"
+#define RAOP_VV "2"
+#define RAOP_VN "65537"
+#define RAOP_AM "AppleTV3,2"
+#define RAOP_SF "0x44"
+#define RAOP_PK "5d6d236b52d18e3ab6d6072fb6e4c7d46bd59ad9cc1947265f00b720fa2c8f66"
+#endif
